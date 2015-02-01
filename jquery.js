1dabd5450166ecebc91b4fbb94a534ac39935943
Alexander Green <j.alexander.green@googlemail.com>
2015-02-01 00:46:20 +0000

    Basic Functionality

---
+++ jquery.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	generateGrid();
+});
+
+
+function generateGrid() {	
+		$("#container").empty();
+		var width = prompt("How many columns wide do you want the Etch-a-Sketch"
+								+ " to be? Enter an integer between 1 and 64");
+		var height = prompt("How many rows high do you want the Etch-a-Sketch"
+								+ " to be? Enter an integer between 1 and 64");
+	
+		width = Math.round(width);
+		height = Math.round(height);
+
+		if (width > 64) {
+			width = 64;
+		};
+		if (width < 1) {
+			width = 1;
+		};
+
+		if (height > 64) {
+			height = 64;
+		};
+		if (height < 1) {
+			height = 1;
+		};
+	
+
+	for (var j = 0; j < height; j++) {
+		var line = $("<div class='line'></div>");
+			
+			for (var i = 0; i < width; i++) {		
+				var square = $("<div></div>").addClass("square");
+				$(line).append(square);		
+			};
+		$("#container").append(line);
+	};
+	var pixels = (960/width) - 2;
+	$(".square").css({"width": pixels, "height":pixels});
+	$(".square").hover(
+		function() {
+			$(this).addClass("highlight");
+		}, function() {
+			$(this).removeClass("highlight");
+		}
+	);
+};
+
+
+	
+
